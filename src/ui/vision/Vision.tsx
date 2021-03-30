333e81960185d6562811a0de5d93db3d62fb5a06
Sam <>
2021-03-30 17:14:54 -0500

    Update Vision.tsx

--- src/ui/vision/Vision.tsx
+++ src/ui/vision/Vision.tsx
@@ -11,10 +11,11 @@ export const Vision = () => {
         const ctx = canvas.getContext('2d')!;
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        
+        ctx.lineJoin = 'round';
+
         if (status?.bbox) {
-            ctx.lineJoin = 'round';
             
+
             ctx.strokeStyle = 'white';
             ctx.lineWidth = 6;
             ctx.strokeRect(status.bbox.x, status.bbox.y, status.bbox.width, status.bbox.height);
@@ -23,6 +24,38 @@ export const Vision = () => {
             ctx.lineWidth = 2;
             ctx.strokeRect(status.bbox.x, status.bbox.y, status.bbox.width, status.bbox.height);
         }
+
+        const midX = 160;
+        const midY = 90;
+        const thiccness = 3;
+
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = thiccness * 2;
+
+        ctx.beginPath();
+        ctx.moveTo(midX - (5 + thiccness), midY);
+        ctx.lineTo(midX + (5 + thiccness), midY);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(midX, midY - (5 + thiccness));
+        ctx.lineTo(midX, midY + (5 + thiccness));
+        ctx.stroke();
+
+        ctx.strokeStyle = 'purple';
+        ctx.lineWidth = 2
+
+        ctx.beginPath();
+        ctx.moveTo(midX - 5, midY);
+        ctx.lineTo(midX + 5, midY);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(midX, midY - 5);
+        ctx.lineTo(midX, midY + 5);
+        ctx.stroke();
+
+        
     })
 
     return <div>
